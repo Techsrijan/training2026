143f9b8603abc79abec0798e10f9015f138c05fd
Techsrijan <aswanibtech@gmail.com>
2026-06-23 12:16:40 +0530

    turtle and string

--- starpattern.py
+++ starpattern.py
@@ -3,9 +3,8 @@ sc=Screen()
 sc.setup(800,400)
 l=['red','green','blue','orange','yellow']
 for i in range(100):
-    pencolor(l[i%5])
-    #pensize(i%10)
+    pencolor(l[i]%5)
     fd(i*10)
-    left(145)
+    left(144)
 
 done()
