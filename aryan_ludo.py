23c2b0a7a45405f2543faa0ef18b84ed1be876ac
aryanyadav <aryanyadav22549@gmail.com>
2026-06-24 10:29:49 +0530

    Aryan yadav

---
+++ aryan_ludo.py
@@ -0,0 +1,730 @@
+from turtle import *
+
+penup()
+speed(0)
+
+goto(-100,320)
+write("Made By Aryan Yadav ",font=("Comic Sans MS",15,"bold"))
+pendown()
+penup()
+goto(-300,-300)
+pendown()
+for i in range(4):
+    fd(600)
+    left(90)
+                             #for blue  box 💙
+fillcolor('blue')
+def sbox():
+
+    begin_fill()
+    for a in range(4):
+        fd(240)
+        left(90)
+    end_fill()
+sbox()
+penup()
+                              #for yellow box 💛
+goto(60,-300)
+pendown()
+fillcolor('yellow')
+sbox()
+penup()
+                              #for green box 💚
+goto(300,60)
+fillcolor('green')
+pendown()
+left(90)
+sbox()
+penup()
+                               # for red box ❤️
+goto(-300,300)
+left(90)
+fillcolor('red')
+pendown()
+left(90)
+sbox()
+
+def ubox():
+    fillcolor('white')
+    begin_fill()
+    for a in range(4):
+        fd(180)
+        left(90)
+    end_fill()
+penup()
+goto(-270,270)
+pendown()
+ubox()
+
+penup()
+goto(-270,-90)
+pendown()
+ubox()
+
+penup()
+goto(90,-90)
+pendown()
+ubox()
+
+penup()
+goto(90,270)
+pendown()
+ubox()
+
+def cbox ():
+    for c in range (4):
+        fd(120)
+        right(90)
+penup()
+goto(60,60)
+pendown()
+cbox()
+
+fillcolor('green')
+begin_fill()
+goto(0,0)
+goto(-60,60)
+goto(60,60)
+end_fill()
+
+fillcolor('yellow')
+begin_fill()
+goto(0,0)
+goto(60,-60)
+goto(60,60)
+end_fill()
+
+penup()
+goto(-60,-60)
+pendown()
+
+fillcolor('red')
+begin_fill()
+goto(0,0)
+goto(-60,60)
+goto(-60,-60)
+end_fill()
+
+fillcolor('blue')
+begin_fill()
+goto(0,0)
+goto(60,-60)
+goto(-60,-60)
+end_fill()
+                        # ❤️1
+penup()
+goto(-245,225)
+pendown()
+fillcolor('red')
+begin_fill()
+circle(22)
+end_fill()
+                        #❤️2
+penup()
+goto(-155,225)
+pendown()
+fillcolor('red')
+begin_fill()
+circle(22)
+end_fill()
+                        #❤️3
+penup()
+goto(-245,140)
+pendown()
+fillcolor('red')
+begin_fill()
+circle(22)
+end_fill()
+                       #❤️
+penup()
+goto(-155,140)
+pendown()
+fillcolor('red')
+begin_fill()
+circle(22)
+end_fill()
+
+                        # ❤💙1
+penup()
+goto(-245,-225)
+pendown()
+fillcolor('blue')
+begin_fill()
+circle(22)
+end_fill()
+                        #❤💙2
+penup()
+goto(-155,-225)
+pendown()
+fillcolor('blue')
+begin_fill()
+circle(22)
+end_fill()
+                        #❤💙3
+penup()
+goto(-245,-140)
+pendown()
+fillcolor('blue')
+begin_fill()
+circle(22)
+end_fill()
+                       #💙4
+penup()
+goto(-155,-140)
+pendown()
+fillcolor('blue')
+begin_fill()
+circle(22)
+end_fill()
+
+                        # 💛1
+penup()
+goto(205,-225)
+pendown()
+fillcolor('yellow')
+begin_fill()
+circle(22)
+end_fill()
+                        #💛️2
+penup()
+goto(115,-225)
+pendown()
+fillcolor('yellow')
+begin_fill()
+circle(22)
+end_fill()
+                        #💛3
+penup()
+goto(205,-140)
+pendown()
+fillcolor('yellow')
+begin_fill()
+circle(22)
+end_fill()
+                       #💛4
+penup()
+goto(115,-140)
+pendown()
+fillcolor('yellow')
+begin_fill()
+circle(22)
+end_fill()
+
+                        # 💚1
+penup()
+goto(205,225)
+pendown()
+fillcolor('green')
+begin_fill()
+circle(22)
+end_fill()
+                        #💚2
+penup()
+goto(115,225)
+pendown()
+fillcolor('green')
+begin_fill()
+circle(22)
+end_fill()
+                        #💚3
+penup()
+goto(205,140)
+pendown()
+fillcolor('green')
+begin_fill()
+circle(22)
+end_fill()
+                       #💚4
+penup()
+goto(115,140)
+pendown()
+fillcolor('green')
+begin_fill()
+circle(22)
+end_fill()
+
+penup()
+goto(60,260)
+pendown()
+fillcolor('green')
+begin_fill()
+for i in range (4):
+
+    fd(40)
+    right(90)
+end_fill()
+
+
+penup()
+goto(20,260)
+pendown()
+fillcolor('green')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(20,220)
+pendown()
+fillcolor('green')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(20,180)
+pendown()
+fillcolor('green')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(20,140)
+pendown()
+fillcolor('green')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(20,100)
+pendown()
+fillcolor('green')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+                    #👹
+
+goto(60,100)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(60,140)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(60,180)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(60,220)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(60,300)
+for i in range (4):
+    fd(40)
+    right(90)
+
+
+penup()
+goto(-220,20)
+pendown()
+fillcolor('red')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(-180,20)
+pendown()
+fillcolor('red')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(-140,20)
+pendown()
+fillcolor('red')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(-100,20)
+pendown()
+fillcolor('red')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(-60,20)
+pendown()
+fillcolor('red')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(-220,60)
+pendown()
+fillcolor('red')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(20,-220)
+pendown()
+fillcolor('blue')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(20,-180)
+pendown()
+fillcolor('blue')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(20,-140)
+pendown()
+fillcolor('blue')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(20,-100)
+pendown()
+fillcolor('blue')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(20,-60)
+pendown()
+fillcolor('blue')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(-20,-220)
+pendown()
+fillcolor('blue')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(220,20)
+pendown()
+fillcolor('yellow')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(180,20)
+pendown()
+fillcolor('yellow')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(140,20)
+pendown()
+fillcolor('yellow')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(100,20)
+pendown()
+fillcolor('yellow')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(260,20)
+pendown()
+fillcolor('yellow')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+
+penup()
+goto(260,-20)
+pendown()
+fillcolor('yellow')
+begin_fill()
+for i in range (4):
+    fd(40)
+    right(90)
+end_fill()
+penup()
+
+goto(-20,100)
+pendown()
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-20,140)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-20,180)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-20,220)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-20,300)
+for i in range (4):
+    fd(40)
+    right(90)
+
+penup()             #😭
+
+goto(-20,-100)
+pendown()
+pendown()
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-20,-140)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-20,-180)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-20,-220)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-20,-260)
+for i in range (4):
+    fd(40)
+    right(90)
+penup()        #💝
+
+goto(60,-100)
+pendown()
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(60,-140)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(60,-180)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(60,-220)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(60,-260)
+for i in range (4):
+    fd(40)
+    right(90)
+
+penup()        # 👌
+goto(-100,60)
+pendown()
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-140,60)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-180,60)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-260,60)
+for i in range (4):
+    fd(40)
+    right(90)
+
+penup()        # 👌
+goto(-100,-20)
+pendown()
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-140,-20)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-180,-20)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(-260,-20)
+for i in range (4):
+    fd(40)
+    right(90)
+
+penup()        # 👌💛
+goto(100,-20)
+pendown()
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(140,-20)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(180,-20)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(260,-20)
+for i in range (4):
+    fd(40)
+    right(90)
+
+penup()        # 👌💛
+goto(100,60)
+pendown()
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(140,60)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(180,60)
+for i in range (4):
+    fd(40)
+    right(90)
+
+goto(260,60)
+for i in range (4):
+    fd(40)
+    right(90)
+
+penup()        # 👌💛
+goto(300,20)
+pendown()
+for i in range (4):
+    fd(40)
+    right(90)
+
+
+def star(x, y):
+    penup()
+    goto(x, y)
+    setheading(0)
+    pendown()
+
+    pencolor("gray")
+    pensize(2)
+
+    for _ in range(5):
+        fd(20)
+        right(144)
+
+# Red safe star
+star(-248, 44)
+
+# Green safe star
+star(36, 242)
+
+# Yellow safe star
+star(232, -40)
+
+# Blue safe star
+star(-48, -240)
+
+# Remaining 4 safe stars
+star(-55, 200)
+star(195, 40)
+star(35, -200)
+star(-205, -40)
+
+
+done()
+
+
+
